71010fd73f4dbf281b68136c0007846a0871ec64
Isaac Yung Lopes <isaaczoi@gmail.com>
2018-07-15 19:50:21 -0300

    Determine semantics

--- lib/nlp/message_analysis.ex
+++ lib/nlp/message_analysis.ex
@@ -1,11 +1,35 @@
 defmodule NLP.MessageAnalysis do
   require Logger
+  alias Gibran.Counter
+  alias Gibran.Tokeniser
 
   def sort_text(text) do
     sentiment = text |> Google.Translate.translate |> Veritaserum.analyze
+    tokenised = Tokeniser.tokenise(text)
 
-    Logger.info IO.inspect(%NLP.MessageStats{type: :question, sentiment: sentiment,match_percentage: 10.0})
+    IO.inspect %NLP.MessageStats{
+      type: semantics(text),
+      sentiment: sentiment,
+      match_percentage: 0,
+      longest_tokens: Counter.longest_tokens(tokenised),
+      most_frequent_tokens: Counter.most_frequent_tokens(tokenised),
+      token_count: Counter.token_count(tokenised),
+      token_density: Counter.token_density(tokenised),
+      uniq_token_count: Counter.uniq_token_count(tokenised),
+      average_chars_per_token: Counter.average_chars_per_token(tokenised)
+    }
 
-    "Foi"
+    "Ola"
+  end
+
+  def semantics(text) do
+    text = String.downcase(text)
+
+    cond do
+      String.match?(text, ~r/.*\!$|.*porque((?!\?).)*$/) -> :interjection
+      String.match?(text, ~r/^(?=.*\bpor\b)(?=.*\bque\b).*$|^como.*|^(?=.*\btem\b)(?=.*\bcomo\b).*$|.*\?$/) -> :question
+      String.match?(text, ~r/valeu|obrigado|agradecido/) -> :greetings
+      true -> :undetermined
+    end
   end
 end
--- lib/nlp/message_stats.ex
+++ lib/nlp/message_stats.ex
@@ -1,3 +1,11 @@
 defmodule NLP.MessageStats do
-  defstruct type: :question, sentiment: 0, match_percentage: 10.0
+  defstruct type: :question,
+            sentiment: 0,
+            match_percentage: 0.0,
+            longest_tokens: 0,
+            most_frequent_tokens: [],
+            token_count: 0,
+            token_density: [],
+            uniq_token_count: [],
+            average_chars_per_token: 0.0
 end
