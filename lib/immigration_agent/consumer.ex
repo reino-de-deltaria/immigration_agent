bd489ac1ff19e7de3a54d48aa25b2ded9c2647d8
Isaac Yung Lopes <isaaczoi@gmail.com>
2018-07-15 11:20:44 -0300

    Domain question

--- lib/immigration_agent/consumer.ex
+++ lib/immigration_agent/consumer.ex
@@ -4,8 +4,9 @@ defmodule Consumer do
 
   def handle_event({:MESSAGE_CREATE, message}, state) do
     Logger.info "Message author: #{message.author.username}, content: #{message.content}"
+
     case message.content do
-      "ping!" -> Message.reply(message, "Ola #{message.author.username}")
+      "Como consigo minha cidadania?" -> Message.reply(message, "Tudo bom #{message.author.username}? Você vai precisar preencher o formulário")
       _ -> :ignore
     end
 
